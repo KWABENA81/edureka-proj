e2f1a7f16564c47dcab2abdb60404a6996b28d31
Solomon Sefa (sask) <kwabena81@yahoo.com>
2022-10-19 15:25:32 -0400

    moving

--- feems/src/main/resources/import.sql
+++ feems/src/main/resources/import.sql
@@ -1,11 +1,19 @@
 create table fee
 (
+    id number not null auto_increment,
     amount DECIMAL not null DEFAULT 0.0,
     feetype VARCHAR not null DEFAULT 'TUITION',
     status  VARCHAR not null DEFAULT 'NONE',
-    sid VARCHAR unique not null
+    sid VARCHAR  not null DEFAULT 'x0x0x0',
+    duedate datetime
 );
-insert into fee (amount, status, sid) values (233.00, 'FULL', '0908976');
-insert into fee (amount, status, sid) values (222.35, 'PARTIAL', '0938976');
-insert into fee (amount, status, sid) values (568.36, 'NONE', '0904976');
-insert into fee (amount, status, sid) values (698.36, 'NONE', '0908676');
+insert into fee (amount, status) values (233.00, 'FULL');
+insert into fee (amount, status) values (222.35, 'PARTIAL');
+insert into fee (amount, status) values (568.36, 'NONE');
+insert into fee (amount, status) values (698.36, 'NONE');
+
+
+
+
+
+
